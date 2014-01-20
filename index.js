080af258fa91a62d50ec9a35bfacf38b88969bdc
Chris Cowan <chriscowan@plus3network.com>
2014-01-20 10:45:27 -0700

    Adding awsbox deps

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var log         = require('./lib/logger');
 var postRequest = require('./lib/postRequest');
 var getRequest  = require('./lib/getRequest');
 var port        = process.env.PORT || 3000;
+var bindAddress = process.env.BIND_ADDRESS || '127.0.0.1'
 
 var server = http.createServer(function (req, res) {
   if (req.method === 'POST') {
@@ -13,7 +14,7 @@ var server = http.createServer(function (req, res) {
   }
 });
 
-server.listen(port, function () {
-  log.info("Starting Server on port %d", port);
+server.listen(port, bindAddress, function () {
+  log.info("Starting Server on http://%s:%d", bindAddress, port);
 });
 
